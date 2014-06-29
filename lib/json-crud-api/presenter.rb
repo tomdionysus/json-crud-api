377079c14ca04898c3caa09bb40121d9d9d7fd9d
Tom Cully <tom.cully@loyalty.co.nz>
2014-06-30 08:35:48 +1200

    Refactored Presenter (code quality)

--- lib/json-crud-api/presenter.rb
+++ lib/json-crud-api/presenter.rb
@@ -29,7 +29,7 @@ module JsonCrudApi
       unless @properties[:parse].has_key? operation
         @properties[:parse][operation] = get_properties(:parse, operation)
       end
-      
+
       out = Hash.new
       data.each_pair { |k,v| out[k] = v if @properties[:parse][operation].include?(k) }
       out
@@ -37,21 +37,27 @@ module JsonCrudApi
 
     def get_properties(method, operation)
       properties = @model.properties.map { |p| p.name.to_sym }
-      unless @exclude.nil?
-        properties -= @exclude[:all] unless @exclude[:all].nil?
-        properties -= @exclude[operation] unless @exclude[operation].nil?
-        unless @exclude[method].nil?
-          properties -= @exclude[method][:all] unless @exclude[method][:all].nil?
-          properties -= @exclude[method][operation] unless @exclude[method][operation].nil?
-        end
+      properties = exclude_properties! properties, method, operation unless @exclude.nil?
+      properties = include_properties! properties, method, operation unless @include.nil?
+      properties
+    end
+
+    def exclude_properties!(properties, method, operation)
+      properties -= @exclude[:all] unless @exclude[:all].nil?
+      properties -= @exclude[operation] unless @exclude[operation].nil?
+      unless @exclude[method].nil?
+        properties -= @exclude[method][:all] unless @exclude[method][:all].nil?
+        properties -= @exclude[method][operation] unless @exclude[method][operation].nil?
       end
-      unless @include.nil?
-        properties += @include[:all] unless @include[:all].nil?
-        properties += @include[operation] unless @include[operation].nil?
-        unless  @include[method].nil?
-          properties += @include[method][:all] unless @include[method][:all].nil?
-          properties += @include[method][operation] unless @include[method][operation].nil?
-        end
+      properties
+    end
+
+    def include_properties!(properties, method, operation)
+      properties += @include[:all] unless @include[:all].nil?
+      properties += @include[operation] unless @include[operation].nil?
+      unless  @include[method].nil?
+        properties += @include[method][:all] unless @include[method][:all].nil?
+        properties += @include[method][operation] unless @include[method][operation].nil?
       end
       properties
     end
